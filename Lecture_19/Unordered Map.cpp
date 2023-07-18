99a39515cda77987c68fec4a7a078dbbaa574589
mubarak747 <124452100+mubarak747@users.noreply.github.com>
2023-07-18 19:42:01 +0530

    Create Unordered Map.cpp

---
+++ Lecture_19/Unordered Map.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+	public:
+		bool hasCycle(ListNode *head){
+			unordered_map<ListNode *, int> mp;
+			ListNode *t = head;
+			
+			while(t){
+				if(mp.count(t->next))return 1;
+					
+				mp[t]++;
+				t = t->next;
+			}
+			return 0;
+		}
+};
